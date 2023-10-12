d9784c5153c6dd93ec03048fb706b36d4a733f40
Hammad <hammadaslam382@gmail.com>
2023-10-12 23:10:20 +0500

    Add NavBar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,16 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-
+import NavLogo from "../../public/assets/logo.png";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Hammad Aslam | Frontend Web & App Developer",
   description:
     "Hey, I am Hammad Aslam a Front-end Web developer I build web and mobile responsive apps using latest state of the art Technology.",
+  icons: {
+    icon: "../../public/assets/logo.png",
+  },
 };
 
 export default function RootLayout({
