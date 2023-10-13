2792e6b213534fbb5749a7bc6a2fc9405a4c219f
Hammad <hammadaslam382@gmail.com>
2023-10-13 19:28:40 +0500

    Add About Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import NavBar from "../../components/NavBar";
 import Main from "../../components/Main";
+import About from "../../components/About";
 
 export default function Home() {
   return (
     <>
       <NavBar />
       <Main />
+      <About />
     </>
   );
 }
