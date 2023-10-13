9a0c3735a528474e60126439a611b736bf29b7fe
Hammad <hammadaslam382@gmail.com>
2023-10-13 19:51:25 +0500

    Add Skills

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import NavBar from "../../components/NavBar";
 import Main from "../../components/Main";
 import About from "../../components/About";
+import Skills from "../../components/Skills";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <NavBar />
       <Main />
       <About />
+      <Skills />
     </>
   );
 }
