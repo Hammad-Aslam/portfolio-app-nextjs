56e280072dc04430411bdc4363e5c824e9bfeb97
Hammad <hammadaslam382@gmail.com>
2023-10-28 00:02:50 +0500

    Add Some Changes

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -7,8 +7,10 @@ import ReactImg from "../public/assets/skills/react.png";
 import Tailwind from "../public/assets/skills/tailwind.png";
 import Github from "../public/assets/skills/github1.png";
 import Typescript from "../public/assets/skills/download.png";
-import NextJS from "../public/assets/skills/Next.png";
-
+import Python from "../public/assets/skills/python3.png";
+import NextJS from "../public/assets/skills/nextjs1.png";
+import shadcnui from "../public/assets/skills/shadcn-ui.png";
+import Shopify from "../public/assets/skills/shopify.png";
 const Skills = () => {
   return (
     <div id="skills" className="w-full lg:h-screen p-2">
@@ -16,8 +18,39 @@ const Skills = () => {
         <p className="text-xl tracking-widest uppercase text-[#5651e5]">
           Skills
         </p>
-        <h2 className="py-4">What I Can Do</h2>
+        <h2 className="py-4">Tech Stack</h2>
         <div className="grid grid-cols-2 lg:grid-cols-4 gap-8">
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image src={Javascript} width={64} height={64} alt="/" />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>JavaScript</h3>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image src={Typescript} width={64} height={64} alt="/" />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Typescript</h3>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image src={Python} width={90} height={90} alt="/" />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Python</h3>
+              </div>
+            </div>
+          </div>
+
           <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
@@ -38,16 +71,7 @@ const Skills = () => {
               </div>
             </div>
           </div>
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image src={Javascript} width={64} height={64} alt="/" />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>JavaScript</h3>
-              </div>
-            </div>
-          </div>
+
           <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
@@ -55,6 +79,26 @@ const Skills = () => {
               </div>
               <div className="flex flex-col items-center justify-center">
                 <h3>React</h3>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="flex items-center justify-center">
+              <div className="m-auto">
+                <Image src={NextJS} width={95} height={95} alt="/" />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Next Js</h3>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image src={shadcnui} width={64} height={64} alt="/" />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Shadcn-UI</h3>
               </div>
             </div>
           </div>
@@ -68,16 +112,7 @@ const Skills = () => {
               </div>
             </div>
           </div>
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image src={Typescript} width={64} height={64} alt="/" />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>Typescript</h3>
-              </div>
-            </div>
-          </div>
+
           <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
@@ -85,16 +120,6 @@ const Skills = () => {
               </div>
               <div className="flex flex-col items-center justify-center">
                 <h3>Github</h3>
-              </div>
-            </div>
-          </div>
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="flex items-center justify-center">
-              <div className="m-auto">
-                <Image src={NextJS} width={600} height={600} alt="/" />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                {/* <h3>Next</h3> */}
               </div>
             </div>
           </div>
--- src/app/cloudinary/page.tsx
+++ src/app/cloudinary/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import React from "react";
-import netflixImg from "../../../public/assets/projects/netflix.jpg";
+import Cloudinary from "../../../public/assets/projects/cloudinary-app.png";
 import { RiRadioButtonFill } from "react-icons/ri";
 import Link from "next/link";
 
@@ -13,12 +13,12 @@ const netflix = () => {
           className="absolute z-1"
           layout="fill"
           objectFit="cover"
-          src={netflixImg}
+          src={Cloudinary}
           alt="/"
         />
         <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2">
-          <h2 className="py-2">Netflix App</h2>
-          <h3>React JS / Tailwind / Firebase</h3>
+          <h2 className="py-2">Cloudniary App</h2>
+          <h3>Next JS / Typescript / Tailwind </h3>
         </div>
       </div>
 
@@ -27,23 +27,23 @@ const netflix = () => {
           <p>Project</p>
           <h2>Overview</h2>
           <p>
-            I built this application in React JS and is hosted on GitHub pages.
-            This app features user authentication with firebase as well as the
-            firestore cloud storage database. This application is pulling movie
-            data from an the IMDB movie API and displaying different categories.
-            It features horizontal sliding and a featured selection. The
-            useContext hook is also being implemented for app-wide state
-            management.
+            Welcome to the Cloudinary Photos App, a full-stack web application
+            built using Cloudinary, Next.js, and Shadcn/ui. This project enables
+            you to manage and showcase your images in a seamless and
+            user-friendly manner without the need for a traditional database.
+            Instead, it leverages Cloudinary's powerful functionality for image
+            management, including uploading, tagging, editing, creating albums,
+            and more.
           </p>
           <a
-            href="https://github.com/fireclint/netflix-react-tailwind"
+            href="https://cloudinary-photos-app-eeaa.vercel.app/gallery"
             target="_blank"
             rel="noreferrer"
           >
             <button className="px-8 py-2 mt-4 mr-8">Code</button>
           </a>
           <a
-            href="https://fireclint.github.io/netflix-react-tailwind/"
+            href="https://cloudinary-photos-app-eeaa.vercel.app"
             target="_blank"
             rel="noreferrer"
           >
@@ -55,7 +55,7 @@ const netflix = () => {
             <p className="text-center font-bold pb-2">Technologies</p>
             <div className="grid grid-cols-3 md:grid-cols-1">
               <p className="text-gray-600 py-2 flex items-center">
-                <RiRadioButtonFill className="pr-1" /> React
+                <RiRadioButtonFill className="pr-1" /> Next
               </p>
               <p className="text-gray-600 py-2 flex items-center">
                 <RiRadioButtonFill className="pr-1" /> Tailwind
@@ -64,10 +64,7 @@ const netflix = () => {
                 <RiRadioButtonFill className="pr-1" /> Javascript
               </p>
               <p className="text-gray-600 py-2 flex items-center">
-                <RiRadioButtonFill className="pr-1" /> Firebase
-              </p>
-              <p className="text-gray-600 py-2 flex items-center">
-                <RiRadioButtonFill className="pr-1" /> IMDB API
+                <RiRadioButtonFill className="pr-1" /> Typescript
               </p>
             </div>
           </div>
